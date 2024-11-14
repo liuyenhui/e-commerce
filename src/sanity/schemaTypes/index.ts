0baca76b44193fb1f69db7087d86a00858531fd8
liuyenhui <liuyenhui@gmail.com>
2024-11-14 19:34:28 +0800

    add header.ts and typegen

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import header from "./section/header";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [header],
+};
---
+++ src/sanity/schemaTypes/section/header.ts
@@ -0,0 +1,29 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "header",
+  title: "Header",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "店铺名称",
+      type: "string",
+    }),
+    defineField({
+      name: "logo",
+      title: "店铺Logo",
+      type: "image",
+    }),
+    defineField({
+      name: "subTitle",
+      title: "副标题",
+      type: "string",
+    }),
+    defineField({
+      name: "background",
+      title: "背景颜色",
+      type: "color",
+    }),
+  ],
+});
